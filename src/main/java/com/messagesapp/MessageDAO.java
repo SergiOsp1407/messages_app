823a691b28f33665371426e68d81fb7c4a6b87e5
Sergio Ospina <sjod1407@gmail.com>
2021-08-27 20:37:43 -0500

    "Create Message" functionality with query to DB.

--- src/main/java/com/messagesapp/MessageDAO.java
+++ src/main/java/com/messagesapp/MessageDAO.java
@@ -2,9 +2,35 @@ package com.messagesapp;
 
 //Here we create the "CRUD" (Create-Read-Update-Delete)
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class MessageDAO {
 
     public static void createMessageDB(Message message){
+
+        Connect db_connect = new Connect();
+        try(Connection cn = db_connect.get_connection()){
+            PreparedStatement ps = null;
+            try{
+                String query = "INSERT INTO messages (message,author_message) VALUES (?,?)";
+                ps = cn.prepareStatement(query);
+
+                ps.setString(1, message.getMessage());
+                ps.setString(2, message.getMessage_author());
+
+                ps.executeUpdate();
+                System.out.println("Message created");
+
+            }catch(SQLException e){
+                System.out.println(e.getMessage());
+            }
+
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+
+        }
 
     }
 
--- src/main/java/com/messagesapp/MessageService.java
+++ src/main/java/com/messagesapp/MessageService.java
@@ -1,9 +1,23 @@
 package com.messagesapp;
 
+import java.util.Scanner;
+
 public class MessageService {
 
     public static void createMessage(){
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write your message");
+        String message = sc.nextLine();
+
+        System.out.println("Who write this message?");
+        String author_name = sc.nextLine();
+
+        Message record = new Message();
+        record.setMessage(message);
+        record.setMessage_author(author_name);
+
+        MessageDAO.createMessageDB(record);
     }
 
     public static void listMessage(){
